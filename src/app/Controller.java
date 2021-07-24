70862eaa3ba52b8a540b00e816293975e64b3fdd
Miguel A. Peña <miapenahu@unal.edu.co>
2021-07-24 07:05:38 -0500

    Fixed opening other windows error

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -77,12 +77,14 @@ public class Controller {
                 test();
             }
         });*/
-//        PauseTransition pause = new PauseTransition(Duration.seconds(1));
-//        editorTextArea.textProperty().addListener(
-//                (observable, oldValue, newValue) -> {
-//                    pause.setOnFinished(event -> test());//doSomething(newValue));
-//                    pause.playFromStart();
-//                });
+        if(editorTextArea != null) {
+            PauseTransition pause = new PauseTransition(Duration.seconds(3));
+            editorTextArea.textProperty().addListener(
+                    (observable, oldValue, newValue) -> {
+                        pause.setOnFinished(event -> test());//doSomething(newValue));
+                        pause.playFromStart();
+                    });
+        }
     }
 
     public void test(){
